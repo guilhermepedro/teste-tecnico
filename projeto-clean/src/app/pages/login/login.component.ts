07c3e1ec0b61633a514c4084aa31663d2a66e461
Guilherme Pedro <guilherme_pedro12@hotmail.com>
2024-01-31 21:49:20 -0300

    fazendo rota de login para menu

--- projeto-clean/src/app/pages/login/login.component.ts
+++ projeto-clean/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/model/usuario';
+import { AutenticacaoService } from 'src/services/Autenticação/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario();
+
+  constructor(private autenticacaoService: AutenticacaoService) { }
+  
+  fazerLogin() {
+    this.autenticacaoService.fazerLogin(this.usuario);
+    
+  }
 
   ngOnInit(): void {
   }
---
+++ projeto-clean/src/services/Autenticação/autenticacao.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/model/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacaoService {
+
+  usuarioAutenticado: boolean = false;
+
+  constructor(private router: Router) { }
+
+  fazerLogin (usuario: Usuario) {
+    if (usuario.nome === 'usuario@gmail.com' &&
+    usuario.senha === '123456') {
+
+      this.usuarioAutenticado = true;
+      this.router.navigate(['/menu']);
+
+    } else {
+      this.usuarioAutenticado = false;
+    }
+
+
+  }
+
+
+
+
+
+}
